a06a808ebc6f462bb896a0f593bfcba4dbf6e7de
webcldev <96698100+webcldev@users.noreply.github.com>
2023-05-17 13:28:42 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  exportPathMap: async function (
-    defaultPathMap,
-    { dev, dir, outDir, distDir, buildId }
-  ) {
-    return {
-      '/': { page: '/' },
-      '/about': { page: '/about' },
-    };
+  experimental: {
+    appDir: true,
   },
+  // ...other configs
 };
 
 module.exports = nextConfig;
