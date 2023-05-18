d02a4a8bed5a217cb1e1ef62a2a3dd58f02aa392
webcldev <webclspa1@gmail.com>
2023-05-18 09:56:45 -0400

    added contact form

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import Image from 'next/image'
 import Landing from '@/components/Landing/Landing'
 import Services from '@/components/Services/Services'
 import Footer from '@/components/Footer/Footer'
+import ContactForm from '@/components/ContactForm/ContactForm'
 export default function Home() {
   return (
     <>
    <Landing/>
    <Services/>
+   <ContactForm/>
    <Footer/>
    </>
   )
---
+++ components/ContactForm/ContactForm.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+
+export default function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+    // Your form handling code here
+  };
+
+  return (
+    <div className="bg-white p-4 rounded-md shadow-md">
+      <form onSubmit={handleSubmit}>
+        <div className="mb-4">
+          <label htmlFor="name" className="block text-sm font-medium text-gray-700">
+            Name
+          </label>
+          <input
+            type="text"
+            name="name"
+            id="name"
+            required
+            value={formData.name}
+            onChange={handleChange}
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            required
+            value={formData.email}
+            onChange={handleChange}
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+
+        <div className="mb-4">
+          <label htmlFor="message" className="block text-sm font-medium text-gray-700">
+            Message
+          </label>
+          <textarea
+            name="message"
+            id="message"
+            required
+            value={formData.message}
+            onChange={handleChange}
+            className="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500 sm:text-sm"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+}
